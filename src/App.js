944742089bc0e99da7acc8f10423deb296ddac28
CornetS28 <corsam28@gmail.com>
2020-09-29 17:16:59 -0400

    create basic pages and link them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
  
 import './App.css';
 
+// Pages
+import mainHome from './pages/mainHome';
+import signup from "./pages/signup";
+import login from "./pages/login";
+import ophtalmologyHome from "./pages/ophtalmologyHome";
+import generalSurgeryHome from "./pages/generalSurgeryHome";
+import urologyHome from "./pages/urologyHome";
+
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={mainHome} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/ophtalmology" component={ophtalmologyHome} />
+          <Route exact path="/urology" component={urologyHome} />
+          <Route
+            exact
+            path="/general-surgery-home"
+            component={generalSurgeryHome}
+          />
+        </Switch>
+      </Router>
+
       <h1> Haiti Hospital </h1>
     </div>
   );
