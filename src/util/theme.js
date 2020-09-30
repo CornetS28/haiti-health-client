f99db5a796e11b2096f2c37418a70924a5b02cd5
CornetS28 <corsam28@gmail.com>
2020-09-30 11:47:32 -0400

    create uitl directory

---
+++ src/util/theme.js
@@ -0,0 +1,19 @@
+export default {
+  palette: {
+    primary: {
+      main: "#2C9823",
+      light: "#757ce8",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+    typography: {
+      useNextVariants: true,
+    },
+  },
+};
