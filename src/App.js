8b5e63ece65c8300400c9320a31a6595f9a3e2e7
CornetS28 <corsam28@gmail.com>
2020-09-29 21:38:39 -0400

    create routes for each created pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
  
 import './App.css';
 
+// Components
+import Navbar from './components/Navbar';
+
 // Pages
 import mainHome from './pages/mainHome';
 import signup from "./pages/signup";
@@ -16,21 +19,22 @@ function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path="/" component={mainHome} />
-          <Route exact path="/signup" component={signup} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/ophtalmology" component={ophtalmologyHome} />
-          <Route exact path="/urology" component={urologyHome} />
-          <Route
-            exact
-            path="/general-surgery-home"
-            component={generalSurgeryHome}
-          />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={mainHome} />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/ophtalmology" component={ophtalmologyHome} />
+            <Route exact path="/urology" component={urologyHome} />
+            <Route
+              exact
+              path="/general-surgery"
+              component={generalSurgeryHome}
+            />
+          </Switch>
+        </div>
       </Router>
-
-      <h1> Haiti Hospital </h1>
     </div>
   );
 }
