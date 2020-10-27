f1d3e4e9aff454da414a7c38d35aee9c386d03ec
CornetS28 <corsam28@gmail.com>
2020-10-27 18:51:22 -0400

    add more static data

--- src/components/modals/FullPatientInfo.js
+++ src/components/modals/FullPatientInfo.js
@@ -27,6 +27,11 @@ const styles = (theme) => ({
   },
   singleInfo: {
     display: "flex",
+    fontSize: 14,
+    marginBottom: -9,
+  },
+  staticText: {
+    fontWeight: "bold",
   },
   userImg: {
     left: "3%",
@@ -34,6 +39,30 @@ const styles = (theme) => ({
   },
   patientHistory: {
     color: theme.palette.primary.main,
+  },
+  section: {
+    marginTop: "5%",
+    margin: "auto",
+    textTransform: "uppercase",
+    color: theme.palette.secondary.main,
+  },
+  section1: {
+    marginBottom: 30,
+    margin: "auto",
+    textTransform: "uppercase",
+    color: theme.palette.secondary.main,
+  },
+  topInfo: {
+    display: "flex",
+  },
+  doctorNote: {
+    backgroundColor: theme.palette.secondary.lightest,
+    height: "97%",
+    width: 330,
+    marginLeft: 60,
+    marginTop: 10,
+    textAlign: "center",
+    borderRadius: '3%',
   },
 });
 
@@ -43,46 +72,34 @@ class FullPatientinfo extends Component {
      const { classes } = this.props;
     return (
       <Grid container spacing={4}>
-        <div className={classes.userImg}>
-          <UserImage />
+        <div className={classes.topInfo}>
+          <div className={classes.userImg}>
+            <UserImage />
+          </div>
+          <div className={classes.doctorNote}>
+            <h4>Latest Doctor Note:</h4>
+            <Divider className={classes.divider} />
+          </div>
         </div>
-
+        <h4 className={classes.section}>Personal Info</h4>
         <Grid item sm={12} xs={12} className={[classes.gridRowWrapper]}>
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-                First Name:{" "}
-                <span className={classes.patientHistory}>James Brooks</span>{" "}
-              </h4>
+              <p>
+                <span className={classes.staticText}>First Name: </span>
+                <span className={classes.patientHistory}>
+                  James Brooks
+                </span>{" "}
+              </p>
             </div>
           </Grid>
+
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-                Last Name: <span className={classes.patientHistory}>Hills</span>
-              </h4>
-            </div>
-          </Grid>
-        </Grid>
-        <Divider className={classes.divider} />
-        <Grid
-          item
-          sm={12}
-          xs={12}
-          className={[classes.gridRowWrapper, classes.spacingTop]}
-        >
-          <Grid item sm={8} xs={12} className="">
-            <div className={classes.singleInfo}>
-              <h4>
-                Age: <span className={classes.patientHistory}>29</span>{" "}
-              </h4>
-            </div>
-          </Grid>
-          <Grid item sm={8} xs={12} className="">
-            <div className={classes.singleInfo}>
-              <h4>
-                Phone: <span className={classes.patientHistory}>+509 2373 2233</span>
-              </h4>
+              <p>
+                <span className={classes.staticText}>Last Name: </span>
+                <span className={classes.patientHistory}>Hills</span>
+              </p>
             </div>
           </Grid>
         </Grid>
@@ -95,17 +112,18 @@ class FullPatientinfo extends Component {
         >
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-                Address:{" "}
-                <span className={classes.patientHistory}>32, Rue Clervau, Petion-ville</span>{" "}
-              </h4>
+              <p>
+                <span className={classes.staticText}>Age: </span>
+                <span className={classes.patientHistory}>29</span>{" "}
+              </p>
             </div>
           </Grid>
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-               Marital Status: <span className={classes.patientHistory}>Maried</span>
-              </h4>
+              <p>
+                <span className={classes.staticText}>Phone: </span>
+                <span className={classes.patientHistory}>+509 2373 2233</span>
+              </p>
             </div>
           </Grid>
         </Grid>
@@ -118,17 +136,314 @@ class FullPatientinfo extends Component {
         >
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-                Emergency Contact:{" "}
-                <span className={classes.patientHistory}>Mrs. Clark Hearald</span>{" "}
-              </h4>
+              <p>
+                <span className={classes.staticText}>Address: </span>
+                <span className={classes.patientHistory}>
+                  32, Rue Clervau, Petion-ville
+                </span>{" "}
+              </p>
             </div>
           </Grid>
           <Grid item sm={8} xs={12} className="">
             <div className={classes.singleInfo}>
-              <h4>
-                Weight: <span className={classes.patientHistory}>Hills</span>
-              </h4>
+              <p>
+                <span className={classes.staticText}>Marital Status: </span>
+                <span className={classes.patientHistory}>Maried</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <h4 className={classes.section1}>Emergency Contact</h4>
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>
+                  Emergency Contact Name:{" "}
+                </span>
+                <span className={classes.patientHistory}>
+                  Mrs. Clark Hearald
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>
+                  Emergency Contact Relation:{" "}
+                </span>{" "}
+                <span className={classes.patientHistory}>Father</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>
+                  Emergency Contact Email:
+                </span>
+                <span className={classes.patientHistory}>
+                  {" "}
+                  father@gmail.com
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Phone: </span>
+                <span className={classes.patientHistory}>+509 2373 2233</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Address: </span>
+                <span className={classes.patientHistory}>
+                  32, Rue Clervau, Petion-ville
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Marital Status: </span>
+                <span className={classes.patientHistory}>Maried</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Emergency Contact: </span>
+                <span className={classes.patientHistory}>
+                  Mrs. Clark Hearald
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Weight: </span>{" "}
+                <span className={classes.patientHistory}>78</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Age: </span>
+                <span className={classes.patientHistory}>29</span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Phone: </span>
+                <span className={classes.patientHistory}>+509 2373 2233</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <h4 className={classes.section1}>Frequent Illnesses</h4>
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Address: </span>
+                <span className={classes.patientHistory}>
+                  32, Rue Clervau, Petion-ville
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Marital Status: </span>
+                <span className={classes.patientHistory}>Maried</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Emergency Contact: </span>
+                <span className={classes.patientHistory}>
+                  Mrs. Clark Hearald
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Weight: </span>{" "}
+                <span className={classes.patientHistory}>78</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Age: </span>
+                <span className={classes.patientHistory}>29</span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Phone: </span>
+                <span className={classes.patientHistory}>+509 2373 2233</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Address: </span>
+                <span className={classes.patientHistory}>
+                  32, Rue Clervau, Petion-ville
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Marital Status: </span>
+                <span className={classes.patientHistory}>Maried</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Emergency Contact: </span>
+                <span className={classes.patientHistory}>
+                  Mrs. Clark Hearald
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Weight: </span>{" "}
+                <span className={classes.patientHistory}>78</span>
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <h4 className={classes.section1}>Diagnostic</h4>
+        <Grid
+          item
+          sm={12}
+          xs={12}
+          className={[classes.gridRowWrapper, classes.spacingTop]}
+        >
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Emergency Contact: </span>
+                <span className={classes.patientHistory}>
+                  Mrs. Clark Hearald
+                </span>{" "}
+              </p>
+            </div>
+          </Grid>
+          <Grid item sm={8} xs={12} className="">
+            <div className={classes.singleInfo}>
+              <p>
+                <span className={classes.staticText}>Weight: </span>{" "}
+                <span className={classes.patientHistory}>78</span>
+              </p>
             </div>
           </Grid>
         </Grid>
