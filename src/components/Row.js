7422b4a23dc5e32c31987dfda864a3f42d5243ff
CornetS28 <corsam28@gmail.com>
2020-10-27 16:12:31 -0400

    fix minor error

--- src/components/Row.js
+++ src/components/Row.js
@@ -23,7 +23,6 @@ import Button from "@material-ui/core/Button";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import CloseIcon from "@material-ui/icons/Close";
-import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import theme from "../util/theme";
 
